8fceb8fdf3fd6587f0f6c3e9bf3575cb28237c7c
lm <l1093015248@163.com>
2019-04-01 18:07:05 +0800

    x

---
+++ src/views/login/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-row type="flex">
+      <el-col :span="24"></el-col>
+    </el-row>
+    <el-row type="flex">
+      <el-col :span="12"></el-col>
+    </el-row>
+    <el-row type="flex">
+      <el-col :span="24"></el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+
+</script>
