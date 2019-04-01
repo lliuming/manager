f8778c7f596cbbd7a53ce8da9189924f2ce789da
lm <l1093015248@163.com>
2019-04-01 18:27:37 +0800

    x

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-import lang from 'element-ui/lib/locale/lang/'
-import locale from 'element-ui/lib/locale'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Element)
+from 'element-ui/lib/locale'
 
 locale.use(lang)
 
